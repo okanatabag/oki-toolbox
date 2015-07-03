a1d0cb8cc1701760a7bc07d00d4653a747ea914b
Okiturk <okanatabag@gmail.com>
2015-07-03 13:27:20 +0300

    Output errors fixed
    Signed-off-by: Okiturk <okanatabag@gmail.com>

--- test/index.php
+++ test/index.php
@@ -1,8 +1,8 @@
 <?php
 include '../okitoolbox.php';
 $otb = new okitoolbox();
-echo '902122224444 Phone number formated ->'.$otb->format_phone("902122224444")."\n";
-echo '35482 int byte formated ->'.$otb->format_bytes("35482")."\n";
-echo '38.5845334,-90.2621693 latitude and longitude converted gps ->'.$otb->calculate_gps(38.5845334, -90.2621693);
-echo '10980.83 converted turkish words >'. $otb->tr_money_to_string(10980.83);
+echo '902122224444 Phone number formated ->'.$otb->format_phone("902122224444")."<br>";
+echo '35482 int byte formated ->'.$otb->format_bytes("35482")."<br>";
+echo '38.5845334,-90.2621693 latitude and longitude converted gps ->';print_r($otb->calculate_gps(38.5845334, -90.2621693)).'<br>';
+echo '10980.83 converted turkish words >'. $otb->tr_money_to_string(10980.83).'<br>';
 ?>
